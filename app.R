b177911fa9c172f8e72ac3b758323026a7e2acd8
Gareth Parris <gareth@prime23.co.uk>
2020-11-16 20:32:38 +0000

    Added Google Tag Manager / Google Analytics

--- app.R
+++ app.R
@@ -11,24 +11,27 @@ server <- function(input, output) {
         
 
         visNetwork(people, connections, width = "160%", height = "150%") %>%
-           #  visIgraphLayout() %>%
-            visEdges(scaling=list(min=4, max=40)) %>%
-            visNodes(scaling=list(min=30)) %>%
-            visOptions(highlightNearest = list(enabled = T, degree = 1, hover = T)) %>%
-            visInteraction(hover=TRUE, zoomView = TRUE,
+            visEdges(scaling=list(min=8, max=40)) %>%
+            visNodes(scaling=list(min=100, max=100)) %>%
+            visOptions(highlightNearest = list(enabled = T, degree = 1, hover = T),
+                       nodesIdSelection=TRUE) %>%
+            visInteraction(hover=TRUE, zoomView = TRUE, 
                            navigationButtons = TRUE,
                            tooltipStyle = 'position: fixed;visibility:hidden;padding: 5px;
                 font-family: sans-serif;font-size:14px;font-color:#000000;background-color: #e3fafa;
                 -moz-border-radius: 3px;-webkit-border-radius: 3px;border-radius: 3px;
                  border: 0px solid #808074;box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
                  max-width:200px;overflow-wrap: normal') %>%
-            visPhysics(solver = "forceAtlas2Based", forceAtlas2Based = list(gravitationalConstant = -50)) %>%
+            visPhysics(solver = "forceAtlas2Based", forceAtlas2Based = list(gravitationalConstant = -150)) %>%
             addFontAwesome() %>%
             visLayout(randomSeed = 02143)
     })
 }
 
 ui <- fluidPage(
+    # Google analytics scripts
+    tags$head(includeHTML(("ga-head.html"))),    
+    tags$body(includeHTML(("ga-body.html"))),    
     # metadata for social sharing
     meta() %>%
         meta_social(
